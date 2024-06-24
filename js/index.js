6f56a861c3b36143734d8030680b2d4df64c85e6
Ryan <coap.ryanmontgomery@gmail.com>
2024-06-24 00:02:49 -0600

    py success

--- js/index.js
+++ js/index.js
@@ -1,55 +1,56 @@
 /*
-  You should be able to call this function with no arguments and see its output in the terminal:
+  You should be able to call this def with no arguments and see its output in the terminal:
   greetProgrammer();
   => "Hello, programmer!"
 */
-function greetProgrammer() {
-  console.log("Hello, programmer!");
+ greetProgrammer(name) {
+  print("Hello, programmer!");
+
 }
 
 /*
-  You should be able to call this function with one argument and see its output in the terminal:
+  You should be able to call this def with one argument and see its output in the terminal:
   greet("Naureen");
   => "Hello, Naureen!"
 */
-function greet(name) {
+def greet(name) {
   console.log(`Hello, ${name}!`);
 }
 
 /*
-  You should be able to call this function with no arguments and see its output in the terminal:
+  You should be able to call this def with no arguments and see its output in the terminal:
   greetWithDefault();
   => "Hello, programmer!"
 
-  You should also be able to call this function with one argument and see its output in the terminal:
+  You should also be able to call this def with one argument and see its output in the terminal:
   greetWithDefault("Sunny");
   => "Hello, Sunny!"
 */
-function greetWithDefault(name = "programmer") {
+def greetWithDefault(name = "programmer") {
   console.log(`Hello, ${name}!`);
 }
 
 /*
-  You should be able to call this function with two arguments and get back its return value:
+  You should be able to call this def with two arguments and get back its return value:
   const sum = add(1, 2);
   console.log(sum);
   => 3
 */
-function add(num1, num2) {
+def add(num1, num2) {
   return num1 + num2;
 }
 
 /*
-  You should be able to call this function with two arguments and get back its return value:
+  You should be able to call this def with two arguments and get back its return value:
   const result = halve(4);
   console.log(result);
   => 2
 
-  If the function is called with an argument that isn't a number, it should return null:
+  If the def is called with an argument that isn't a number, it should return null:
   const result = halve("two")
   => null
 */
-function halve(number) {
+def halve(number) {
   if (typeof number !== "number") return null;
 
   return number / 2;
--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+greet_programmer()
+pass
+        
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet({"Guido"})
+pass
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default()
+pass
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+add(1, 3)
+pass
 
 def halve(number):
-    pass
+    return number / 2
+    
+pass
